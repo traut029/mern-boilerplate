dc9d9a98186d110e4461bad4c2c7960e5b471afd
Eric Trautmiller <trautmillereahs@gmail.com>
2018-05-23 13:09:34 -0500

    update

---
+++ models/Article.js
@@ -0,0 +1,14 @@
+//This is a basic example of how a basic model is structured
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const articleSchema = new Schema({
+//   title: { type: String, required: true },
+//   url: { type: String, required: true },
+//   date: { type: String, required:true }
+// });
+
+// const Article = mongoose.model("Article", articleSchema);
+
+// module.exports = Article;
